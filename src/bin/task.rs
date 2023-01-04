5752ff961c64f8fc9669025e53e676a28f66b211
yud0uhu <cist.b219.y.osaka@gmail.com>
2023-01-04 14:34:28 +0900

    :+1: サンプルコードと依存クレートを追加

---
+++ src/bin/task.rs
@@ -0,0 +1,73 @@
+#![no_std]
+#![no_main]
+#![feature(abi_avr_interrupt)]
+#![feature(alloc_error_handler)]
+
+// use arduino_hal::port::mode::Output;
+// use arduino_hal::port::Pin;
+use arduino_hal::prelude::*;
+// use avr_device::atmega328p::tc1::tccr1b::CS1_A;
+// use avr_device::atmega328p::TC1;
+// use core::mem;
+use panic_halt as _;
+use ufmt::{uWrite};
+use panic_halt as _;
+use core::ops::Range;
+// use arduino_hal::port::{mode, Pin};
+// use either::*;
+
+enum TaskState {
+    RUNNING,
+    READY,
+    SUSPEND
+}
+struct Task {
+    task_id: u32,
+    task_state: TaskState,
+    task_priority: u16,
+}
+
+
+fn blink_for_range<W: uWrite<Error = void::Void>>(range : Range<u16>, serial: &mut W){
+    let task = Task {
+        task_id: 1,
+        task_state: TaskState::READY,
+        task_priority: 2,
+    };
+    let ticks = task.task_priority;
+    
+    range.map(|i| i * 100).for_each(|ms| {
+        ufmt::uwriteln!(
+            serial,
+            "configuring timer output compare register = {}",
+            ticks
+        )
+        .void_unwrap();
+        ufmt::uwriteln!(serial, "Hello from Arduino!\r").void_unwrap();
+        arduino_hal::delay_ms(ms as u16);
+    });
+}
+
+#[avr_device::interrupt(atmega328p)]
+fn TIMER0_OVF() {
+    arduino_hal::delay_ms(100);
+}
+
+#[arduino_hal::entry]
+fn main() -> ! {
+
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+    dp.EXINT.eicra.modify(|_, w| w.isc0().bits(0x02));
+    // Enable the INT0 interrupt source.
+    dp.EXINT.eimsk.modify(|_, w| w.int0().set_bit());
+
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);    
+    
+    loop {
+        blink_for_range(0..10,&mut serial);
+        unsafe { 
+            avr_device::interrupt::enable();
+         };
+    }
+}
---
+++ src/bin/uno-spi-feedback.rs
@@ -0,0 +1,49 @@
+/*!
+ * Example of the SPI bus, by looping back output to input.
+ *
+ * This example demonstrates how to set up a SPI interface and communicate
+ * over it.  The physical hardware configuation consists of connecting a
+ * jumper directly from pin `D11` to pin `D12`.
+ *
+ * If done correctly, you should see it output the line `data: 15` repeatedly (aka 0b00001111).  If
+ * the output you see is `data: 255`, you may need to check your jumper.
+ *
+ * Connections:
+ *  - `D11` connected directly to `D12` (loop MOSI to MISO)
+ */
+#![no_std]
+#![no_main]
+
+use arduino_hal::prelude::*;
+use arduino_hal::spi;
+use embedded_hal::spi::FullDuplex;
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    // set up serial interface for text output
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    // Create SPI interface.
+    let (mut spi, _) = arduino_hal::Spi::new(
+        dp.SPI,
+        pins.d13.into_output(),
+        pins.d11.into_output(),
+        pins.d12.into_pull_up_input(),
+        pins.d10.into_output(),
+        spi::Settings::default(),
+    );
+
+    loop {
+        // Send a byte
+        nb::block!(spi.send(0b00001111)).void_unwrap();
+        // Because MISO is connected to MOSI, the read data should be the same
+        let data = nb::block!(spi.read()).void_unwrap();
+
+        ufmt::uwriteln!(&mut serial, "data: {}\r", data).void_unwrap();
+        arduino_hal::delay_ms(1000);
+    }
+}
