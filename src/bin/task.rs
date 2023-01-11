0bf66b5cf44731a7bda724f7fa07581c25be634d
yud0uhu <cist.b219.y.osaka@gmail.com>
2023-01-11 12:49:59 +0900

    :shower: format

--- src/bin/task.rs
+++ src/bin/task.rs
@@ -9,17 +9,17 @@ use arduino_hal::prelude::*;
 // use avr_device::atmega328p::tc1::tccr1b::CS1_A;
 // use avr_device::atmega328p::TC1;
 // use core::mem;
-use panic_halt as _;
-use ufmt::{uWrite};
-use panic_halt as _;
 use core::ops::Range;
+use panic_halt as _;
+use panic_halt as _;
+use ufmt::uWrite;
 // use arduino_hal::port::{mode, Pin};
 // use either::*;
 
 enum TaskState {
     RUNNING,
     READY,
-    SUSPEND
+    SUSPEND,
 }
 struct Task {
     task_id: u32,
@@ -27,15 +27,14 @@ struct Task {
     task_priority: u16,
 }
 
-
-fn blink_for_range<W: uWrite<Error = void::Void>>(range : Range<u16>, serial: &mut W){
+fn blink_for_range<W: uWrite<Error = void::Void>>(range: Range<u16>, serial: &mut W) {
     let task = Task {
         task_id: 1,
         task_state: TaskState::READY,
         task_priority: 2,
     };
     let ticks = task.task_priority;
-    
+
     range.map(|i| i * 100).for_each(|ms| {
         ufmt::uwriteln!(
             serial,
@@ -55,19 +54,18 @@ fn TIMER0_OVF() {
 
 #[arduino_hal::entry]
 fn main() -> ! {
-
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
     dp.EXINT.eicra.modify(|_, w| w.isc0().bits(0x02));
     // Enable the INT0 interrupt source.
     dp.EXINT.eimsk.modify(|_, w| w.int0().set_bit());
 
-    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);    
-    
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+
     loop {
-        blink_for_range(0..10,&mut serial);
-        unsafe { 
+        blink_for_range(0..10, &mut serial);
+        unsafe {
             avr_device::interrupt::enable();
-         };
+        };
     }
 }
